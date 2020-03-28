25dda50700119b83cffbc73d93d243074c686d47
Meaghanaa <59702283+Meaghanaa@users.noreply.github.com>
2020-03-28 15:10:20 -0400

    Add files via upload

---
+++ Bolt.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Bolt : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * speed;
+    }
+}
